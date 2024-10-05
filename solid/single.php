28d0c873f777da9775e53bc5bd96f84215ac74b4
ingk-18 <readog1224@yahoo.co.jp>
2024-10-05 13:41:42 +0900

    単一原則

---
+++ solid/single.php
@@ -0,0 +1,42 @@
+<?php
+
+
+// 悪い例
+class Payment
+{
+    public function orderByCash($price){}
+    public function cancelByCash($price){}
+
+    public function orderByCreditCard($price){}
+    public function cancelByCreditCard($price){}
+}
+
+// 良い例
+
+class Payment {
+    private $paymentType;
+
+    public function __construct($paymentType) {
+        $this->paymentType = $paymentType;
+    }
+
+    public function order($price) {
+        $this->paymentType->order($price);
+    }
+
+    public function cancel($price) {
+        $this->paymentType->cancel($price);
+    }
+}
+
+class Cash {
+    public function order($price){}
+    public function cancel($price){}
+}
+
+class CreditCard {
+    public function order($price){}
+    public function cancel($price){}
+}
+
+
